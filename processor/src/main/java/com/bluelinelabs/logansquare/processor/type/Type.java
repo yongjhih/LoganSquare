1fec4d526efb63dc1e5ca5cf37f8cdb84e388a80
Andrew Chen <yongjhih@gmail.com>
2015-06-17 02:28:09 +0800

    Allow parse list with custom TypeConverter #60

--- processor/src/main/java/com/bluelinelabs/logansquare/processor/type/Type.java
+++ processor/src/main/java/com/bluelinelabs/logansquare/processor/type/Type.java
@@ -25,11 +25,12 @@ public abstract class Type {
     public static Type typeFor(TypeMirror typeMirror, TypeMirror typeConverterType, Elements elements, Types types) {
         TypeMirror genericClassTypeMirror = types.erasure(typeMirror);
 
-        boolean hasTypeConverter = typeConverterType != null && !typeConverterType.toString().equals("void");
+        //boolean hasTypeConverter = typeConverterType != null && !typeConverterType.toString().equals("void");
         boolean isCollection = !genericClassTypeMirror.toString().equals(typeMirror.toString()) || (typeMirror instanceof ArrayType);
 
-        if (!hasTypeConverter && isCollection) {
-            return ContainerType.containerTypeFor(typeMirror, genericClassTypeMirror, elements, types);
+        //if (!hasTypeConverter && isCollection) {
+        if (isCollection) {
+            return ContainerType.containerTypeFor(typeMirror, typeConverterType, genericClassTypeMirror, elements, types);
         } else {
             return FieldType.fieldTypeFor(typeMirror, typeConverterType, elements, types);
         }
